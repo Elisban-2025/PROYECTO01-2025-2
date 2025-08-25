2d099e533b75d6f5168a8fceec75f3d338aca892
Elisban-2025 <elisbanhuaylla@gmail.com>
2025-08-25 01:24:25 -0500

    Add files via upload

---
+++ MiPrimeraclase/pe/edu/upeu/Persona.java
@@ -0,0 +1,25 @@
+package MiPrimeraclase.pe.edu.upeu;
+
+public class Persona {
+
+	private int nombre;
+	private int dni;
+	private int lamma;
+	private int genero;
+
+	public void caminar() {
+		// TODO - implement Persona.caminar
+		throw new UnsupportedOperationException();
+	}
+
+	public void comer() {
+		// TODO - implement Persona.comer
+		throw new UnsupportedOperationException();
+	}
+
+	public void operation() {
+		// TODO - implement Persona.operation
+		throw new UnsupportedOperationException();
+	}
+
+}
