70dda5e3bf23b3d8f6310203e7b96d98e1228290
Elisban-2025 <elisbanhuaylla@gmail.com>
2025-08-25 01:15:29 -0500

    Add files via upload

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
@@ -0,0 +1,20 @@
+package pe.edu.upeu.asistencia.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AsistenciaController {
+
+    @FXML private TextField txtNum1, txtNum2;
+
+    @FXML private Label result;
+
+    @FXML
+    private void sumar() {
+        double num1 = Double.parseDouble(txtNum1.getText());
+        double num2 = Double.parseDouble(txtNum2.getText());
+        double resultado = num1 + num2;
+        result.setText(String.valueOf(resultado));
+    }
+}
