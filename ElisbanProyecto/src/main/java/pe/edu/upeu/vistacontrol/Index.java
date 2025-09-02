fad823adc4327c85dc93fe8e38adc93abd2a9bfe
Elisban-2025 <elisbanhuaylla@gmail.com>
2025-09-01 22:11:54 -0500

    Add files via upload
    PROYECTO FINALIZADO
    PRIMERA UNIDAD 2025

---
+++ ElisbanProyecto/src/main/java/pe/edu/upeu/vistacontrol/Index.java
@@ -0,0 +1,11 @@
+package pe.edu.upeu.vistacontrol;
+
+public class Index {
+    public static void inicio() {
+        Indexprincipal.inicio();
+    }
+
+    public static void main(String[] args) {
+        inicio();
+    }
+}
